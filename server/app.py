25a3936db6c6406407071e71e5611b14eff4d569
iz4nam1 <anusnamsp2005@gmail.com>
2026-04-09 01:10:54 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,4 +1,10 @@
 from main import app
+import uvicorn
 
 def main():
     return app
+
+# THIS is what they are crying about
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=7860)
+    
